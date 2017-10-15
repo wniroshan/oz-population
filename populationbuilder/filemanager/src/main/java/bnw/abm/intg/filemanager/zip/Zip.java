744ef5fea5b573cf9cd1c7f411ba8cff50e472a7
wniroshan <wniroshan@gmail.com>
2017-10-16 09:59:12 +1100

    transfer to github

---
+++ populationbuilder/filemanager/src/main/java/bnw/abm/intg/filemanager/zip/Zip.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package bnw.abm.intg.filemanager.zip;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import bnw.abm.intg.filemanager.Find;
+import bnw.abm.intg.util.BNWLogger;
+
+/**
+ * @author Bhagya N. Wickramasinghe
+ *
+ */
+public class Zip {
+	private static final Logger LOGGER = BNWLogger.getLogger();
+	private static List<FileSystem> fileSystems = new ArrayList<>();
+
+	public static Reader read(Path zipFile, String fileInZip) throws IOException {
+		FileSystem fs = FileSystems.newFileSystem(zipFile, null);
+		return Files.newBufferedReader(fs.getPath(fileInZip));
+	}
+
+	public static void create(Path zipFile, List<Path> componentFiles) throws IOException, URISyntaxException {
+		Map<String, String> env = new HashMap<>();
+		env.put("create", "true");
+		// locate file system by using the syntax
+		// defined in java.net.JarURLConnection
+		URI uri = new URI("jar:" + zipFile.toUri());
+
+		try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
+			for (Path externalFilePath : componentFiles) {
+				Path filePathInZip = zipfs.getPath(externalFilePath.getFileName().toString());
+				Files.copy(externalFilePath, filePathInZip, StandardCopyOption.REPLACE_EXISTING);
+			}
+		}
+	}
+
+	public static List<Path> findFiles(Path jarFile, String pattern) throws IOException {
+		
+		Path currDir = Paths.get("").toAbsolutePath();
+		jarFile = currDir.resolve(jarFile);
+		
+		HashMap<Path, FileSystem> fileSystems = new HashMap<Path, FileSystem>();
+		FileSystem fs = fileSystems.get(jarFile);
+		
+
+		if (fs == null) {
+			// throw new FileNotFoundException(jarFile.toString());
+			Map<String, String> env = new HashMap<>();
+			env.put("create", "false");
+			try {
+				fs = FileSystems.newFileSystem(URI.create("jar:file:" + jarFile), env);
+			} catch (FileSystemNotFoundException iae) {
+				throw new FileNotFoundException(jarFile.toString());
+			}
+			fileSystems.put(jarFile, fs);
+		}
+
+		Find finder = new Find(pattern, fs);
+
+		for (Path root : fs.getRootDirectories()) {
+			Files.walkFileTree(root, finder);
+		}
+
+		ArrayList<Path> files = finder.getFilePaths();
+		Zip.fileSystems.add(fs);
+		return files;
+	}
+
+	public static void closeOpenedFileSystems() throws IOException {
+		for (FileSystem fs : fileSystems) {
+			fs.close();
+		}
+	}
+}
